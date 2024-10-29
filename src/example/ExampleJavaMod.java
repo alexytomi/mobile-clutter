85deb4b4ff8e1754436898ddf38c8f23d512f42c
alexytomi <60690056+alexytomi@users.noreply.github.com>
2024-10-29 09:48:31 +0800

    fix crash

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -35,31 +35,36 @@ public class ExampleJavaMod extends Mod {
     // break things on other platforms
     // Events.on(ClientLoadEvent.class, null);
 
-    Events.on(WorldLoadEvent.class, e -> {
-      buildCoreItemsDisplay((Table) mobileUI);
-    });
   }
 
   private void createInGameGroup() {
     Log.info("Loading UI stuff");
     // don't mind me blatantly copying code from HudFragment on how it builds the
     // coreinfo table
-    this.mobileUI.fill(mobileUI -> {
-      // for future me, this is where you put your UI stuff so they go in the mobileUI
-      // WidgetGroup instead of hudGroup which IMO is jank. Maybe make another
-      // WidgetGroup for non-gameplay UI elements in the future.
-      mobileUI.top();
-      if (Core.settings.getBool("macnotch"))
-        mobileUI.margin(Vars.macNotchHeight);
-      mobileUI.name = "ClutterUI";
-      mobileUI.visible(() -> true);
-      // pauseHeight is 36f according to
-      // mindustry.ui.fragments.HudFragment.pauseheght. It is private and final so I
-      // cannot call it.
-      
+    Events.run(Trigger.update, () -> {
 
+      Log.info("update");
+      this.mobileUI.fill(mobileUI -> {
+        mobileUIProperties(mobileUI);
+        buildCoreItemsDisplay(mobileUI);
+
+      });
     });
 
+  }
+
+  private void mobileUIProperties(Table mobileUI) {
+    // for future me, this is where you put your UI stuff so they go in the mobileUI
+    // WidgetGroup instead of hudGroup which IMO is jank. Maybe make another
+    // WidgetGroup for non-gameplay UI elements in the future.
+    mobileUI.top();
+    if (Core.settings.getBool("macnotch"))
+      mobileUI.margin(Vars.macNotchHeight);
+    mobileUI.name = "ClutterUI";
+    mobileUI.visible(() -> true);
+    // pauseHeight is 36f according to
+    // mindustry.ui.fragments.HudFragment.pauseheght. It is private and final so I
+    // cannot call it.
   }
 
   private void buildCoreItemsDisplay(Table mobileUI) {
